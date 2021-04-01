ab6db6acf730a1eaef412604f2c18251056a9841
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 12:11:27 +0300

    Add the title and sub_title fields.

--- locations/models.py
+++ locations/models.py
@@ -6,6 +6,8 @@ class Locations(models.Model):
     location_title = models.CharField(max_length = 50, unique = True)
     location_content = models.TextField()
     featured_image = models.ImageField(null = True)
+    title = models.CharField(max_length = 50, null = True, unique = True)
+    sub_title = models.CharField(max_length = 200, null = True, unique = True)
 
     def __str__(self):
         return self.location_title
