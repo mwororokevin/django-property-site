d8eb7beffb2fed933d1863e0e5a8c2ddeac6f58a
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 12:18:08 +0300

    Add the employees app in the installed apps list.

--- RealEstateApp/settings.py
+++ RealEstateApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'listing.apps.ListingConfig',
     'locations.apps.LocationsConfig',
+    'employees.apps.EmployeesConfig',
 ]
 
 MIDDLEWARE = [
