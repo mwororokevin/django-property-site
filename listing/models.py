1cfd13ac3a8cab56b26603dffacf53f431fec98a
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 11:34:49 +0300

    Replace the listing_slug with listing_content.

--- listing/models.py
+++ listing/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    listing_title = models.CharField(max_length = 200, unique = True)
-    listing_slug = models.SlugField(max_length = 200, unique = True)
+    listing_title   = models.CharField(max_length = 200, unique = True)
+    listing_content = models.TextField()
 
     def __str__(self):
         return self.listing_title
