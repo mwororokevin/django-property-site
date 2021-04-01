8a81812eb832455ef6a0171aaf4fdb3ec20aa1c8
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 11:42:57 +0300

    Add the location class to the models.py file.
    This is for the Location app.

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Locations(models.Model):
+    location_title = models.CharField(max_length = 50, unique = True)
+    location_content = models.TextField()
+
+    def __str__(self):
+        return self.location_title
