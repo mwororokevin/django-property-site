dd8c751e91e7c67e07d82682cbebb530c4e43154
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 11:54:57 +0300

    Add a default value for the listing_content.

--- listing/models.py
+++ listing/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+# from ..locations import Locations
 
 # Create your models here.
 
 class Post(models.Model):
     listing_title   = models.CharField(max_length = 200, unique = True)
-    listing_content = models.TextField()
+    listing_content = models.TextField(default="***")
 
     def __str__(self):
         return self.listing_title
