d48cc2cdc855ba82a0d3075a8af777cd6ea60d88
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 11:38:11 +0300

    Add the locations app to the Installed_Apps list.

--- RealEstateApp/settings.py
+++ RealEstateApp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listing.apps.ListingConfig'
+    'listing.apps.ListingConfig',
+    'locations.apps.LocationsConfig',
 ]
 
 MIDDLEWARE = [
