5ef429e3a26200104a71c68067340bd9083857ee
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 16:34:49 +0300

    Add additional models to the Listing class.

--- listing/models.py
+++ listing/models.py
@@ -19,6 +19,19 @@ LISTING_STATUS = (
     ('Pending', 'Pending')
 )
 
+LISTING_TYPE = (
+    ('Apartments', 'Apartments'),
+    ('Commercial', 'Commercial'),
+    ('Hotels & Lodges', 'Hotels & Lodges'),
+    ('Land/Plots', 'Land/Plots'),
+    ('Other', 'Other')
+)
+
+FURNISHED_STATUS = (
+    ('Furnished', 'Yes'),
+    ('Unfurnished', 'No')
+)
+
 class Listing(models.Model):
     listing_title   = models.CharField(max_length = 200, unique = True)
     listing_content = models.TextField(null = True)
@@ -37,7 +50,105 @@ class Listing(models.Model):
         default = "For Rent"
     )
 
+    listing_type = models.CharField(
+        max_length = 15,
+        choices = LISTING_TYPE,
+        default = 'Apartments',
+        help_text = 'Select the correct type that represents this listing.'
+    )
+
+    number_of_bedrooms = models.IntegerField(
+        blank = False,
+        help_text = 'Enter the total number of Bedrooms in this listing.'
+    )
+
+    number_of_bathrooms = models.IntegerField(
+        blank = False,
+        help_text = 'Enter the total number of bathrooms in this listing.'
+    )
+
+    furniture = models.CharField(
+        max_length = 15,
+        choices = FURNISHED_STATUS,
+        default = 'Yes'
+    )
+
+    service_fee = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing have a service fee?'
+    )
+
+    air_conditioning = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing have an AC unit or similar?'
+    )
+
+    garden = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing come with a garden?'
+    )
+
+    swimming_pool = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing include a swimming pool?'
+    )
+
+    balcony_or_terrace = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing include a balcony or a terrace?'
+    )
+
+    gym = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing include a gym?'
+    )
+
+    internet = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Is there a pre-existing internet connection in this listing?'
+    )
+
+    dstv_or_cable = models.CharField(
+        max_length = 100,
+        blank = True,
+        help_text = 'Does this listing come with a DSTV / Cable connection?'
+    )
+
+    water_information = models.CharField(
+        max_length = 100,
+        blank = False,
+        help_text = 'Enter information about the water supply.'
+    )
+
+    power_information = models.CharField(
+        max_length = 100,
+        blank = False,
+        help_text = 'Enter information about the power/energy supply.'
+    )
+
+    security_information = models.CharField(
+        max_length = 100,
+        blank = False,
+        help_text = 'Enter information about the security situation.'
+    )
+
+    parking_information = models.CharField(
+        max_length = 100,
+        blank = False,
+        help_text = 'Enter information about the parking situation.'
+    )
+
+    kitchen_appliances = models.CharField(
+        max_length = 50,
+        help_text = 'Select the appliances that are available in the current listing.'
+    )
+
     def __str__(self):
         return self.listing_title
-
-
