45c4f18b2e57b77dc85989c6820b307440c19810
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-01 13:18:05 +0300

    Adding the Location tuple and listing location model.

--- listing/models.py
+++ listing/models.py
@@ -3,9 +3,24 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
+LOCATION = (
+    ('Nairobi', 'Nairobi'),
+    ('Kisumu', 'Kisumu'),
+    ('Mombasa', 'Mombasa'),
+    ('Thika', 'Thika'),
+    ('Nakuru', 'Nakuru')
+)
+
+class Listing(models.Model):
     listing_title   = models.CharField(max_length = 200, unique = True)
     listing_content = models.TextField(null = True)
+
+    listing_location = models.CharField(
+        max_length = 50,
+        choices = LOCATION, 
+        default = "", null = 
+        False
+    )
 
     def __str__(self):
         return self.listing_title
