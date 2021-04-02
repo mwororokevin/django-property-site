3bba73411f833b8ebbb0a9b9afad2e5c86874ab6
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-02 12:06:56 +0300

    Rename the last two variables in the list_display due to changes in the models.py file.

--- locations/admin.py
+++ locations/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Location
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('location_title', 'title', 'sub_title')
+    list_display = ('location_title', 'header_title', 'header_caption')
 
 admin.site.register(Location, LocationAdmin)
