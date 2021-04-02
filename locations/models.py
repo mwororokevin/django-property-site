20f21d3f877e78ba19815e92217f0d2d177521a8
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-04-02 11:53:51 +0300

    Rename two of the models to header_title and header_caption.
    It will also introduce help text and change the unique attribute to
    false for both.

--- locations/models.py
+++ locations/models.py
@@ -6,8 +6,14 @@ class Location(models.Model):
     location_title = models.CharField(max_length = 50, unique = True)
     location_content = models.TextField()
     featured_image = models.ImageField(null = True)
-    title = models.CharField(max_length = 50, null = True, unique = True)
-    sub_title = models.CharField(max_length = 200, null = True, unique = True)
+    header_title = models.CharField(
+        max_length = 50, null = True, unique = False,
+        help_text = "If you don't want to use the main title in the page header, enter a new one here."
+    )
+    header_caption = models.CharField(
+        max_length = 200, null = True, unique = False,
+        help_text = "If you wish to display a caption under the title, enter it here."
+    )
 
     def __str__(self):
         return self.location_title
